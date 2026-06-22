b35924c0d8b1b9de46a44f864a52a0b9d5d8127c
itsemanuela <notemanu152@gmail.com>
2026-06-22 16:18:24 +0200

    Prime classi istanziate (Distributore, Punto emissione, Rivenditore ed Enum Stato-Distributore

---
+++ src/test/java/itsemanuela/Distributore.java
@@ -0,0 +1,36 @@
+package itsemanuela;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="distributore")
+public class Distributore extends Punto_di_Emissione {
+    Stato_Distributore stato;
+
+    public Distributore() {
+        super();
+    }
+
+    public Distributore(Stato_Distributore stato, String indirizzo) {
+        super(indirizzo);
+        this.stato = stato;
+
+    }
+
+    //getter e setter
+    public Stato_Distributore getStato() {
+        return stato;
+    }
+    public void setStato(Stato_Distributore stato) {
+        this.stato = stato;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Distributore{");
+        sb.append("stato=").append(stato);
+        sb.append('}');
+        return sb.toString();
+    }
+}
