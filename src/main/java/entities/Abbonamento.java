2f9d41dc8d6d843d4623400d4e623134da8a3bf1
cafagnarob <cafagna.rob@gmail.com>
2026-06-22 16:14:40 +0200

    add class

--- src/main/java/entities/Abbonamento.java
+++ src/main/java/entities/Abbonamento.java
@@ -1,4 +1,61 @@
 package entities;
 
+import Enum.TipoAbbonamento;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+import java.time.LocalDate;
+
+@Entity
 public class Abbonamento extends Titolo_di_viaggio {
+    @Column(nullable = false)
+    private TipoAbbonamento tipo;
+
+    @Column(nullable = false)
+    private LocalDate data_di_scadenza;
+
+    @Column(nullable = false)
+    private Tessera tessera;
+
+    public Abbonamento() {
+
+    }
+
+    public Abbonamento(Long id, Punto_di_emissione luogo_di_emissione, double prezzo, TipoAbbonamento tipo,
+                       Tessera tessera) {
+        super(id, luogo_di_emissione, prezzo);
+        this.tipo = tipo;
+        if (tipo == TipoAbbonamento.SETTIMANALE) {
+            this.data_di_scadenza = getData_di_emissione().plusWeeks(1);
+        } else {
+            this.data_di_scadenza = getData_di_emissione().plusMonths(1);
+        }
+        this.tessera = tessera;
+    }
+
+    public LocalDate getData_di_scadenza() {
+        return data_di_scadenza;
+    }
+
+    public Tessera getTessera() {
+        return tessera;
+    }
+
+    public TipoAbbonamento getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoAbbonamento tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Abbonamento{ \n" +
+                super.toString() +
+                "tipo=" + tipo + "\n" +
+                ", data_di_scadenza=" + data_di_scadenza + "\n" +
+                ", tessera=" + tessera + "\n" +
+                "}  \n";
+    }
 }
--- src/main/java/entities/Biglietto.java
+++ src/main/java/entities/Biglietto.java
@@ -1,4 +1,35 @@
 package entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
 public class Biglietto extends Titolo_di_viaggio {
+    @Column(nullable = false)
+    private boolean vidimato;
+
+    public Biglietto() {
+    }
+
+    public Biglietto(Long id, Punto_di_emissione luogo_di_emissione, double prezzo, boolean vidimato) {
+        super(id, luogo_di_emissione, prezzo);
+        this.vidimato = false;
+
+    }
+
+    public boolean getVidimato() {
+        return vidimato;
+    }
+
+    public void setVidimato(boolean vidimato) {
+        this.vidimato = vidimato;
+    }
+
+    @Override
+    public String toString() {
+        return "Biglietto{ \n"
+                + super.toString() + "\n" +
+                "vidimato=" + vidimato + "\n" +
+                "} \n";
+    }
 }
