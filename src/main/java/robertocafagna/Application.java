6128ba28b4ee3c3a2ceddccf9f4b3b73ee4349a5
cafagnarob <cafagna.rob@gmail.com>
2026-06-22 15:14:18 +0200

    add factory

--- src/main/java/robertocafagna/Application.java
+++ src/main/java/robertocafagna/Application.java
@@ -1,8 +1,17 @@
 package robertocafagna;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
 
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("u4-w3-d5pu");
+
+
     public static void main(String[] args) {
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
         System.out.println("Hello World!");
     }
 }
