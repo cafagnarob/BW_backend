8e394cf20c9ef4ef03f46e6fc0cedd80660c89e3
itsemanuela <notemanu152@gmail.com>
2026-06-23 17:53:51 +0200

    Aggiunta ad application dei punti di emissione

--- src/main/java/robertocafagna/Application.java
+++ src/main/java/robertocafagna/Application.java
@@ -1,9 +1,14 @@
 package robertocafagna;
 
+import dao.PuntoDiEmissioneDAO;
 import dao.UtenteDAO;
+import entities.Distributore;
+import entities.PuntoDiEmissione;
+import entities.Rivenditore;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import Enum.StatoDistributore;
 
 public class Application {
 
@@ -14,6 +19,8 @@ public class Application {
 
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         UtenteDAO utenteDAO = new UtenteDAO(entityManager);
+        PuntoDiEmissioneDAO dao = new PuntoDiEmissioneDAO(entityManager);
+
 
         // CREO NUOVI UTENTI
        /* Utente u1 = new Utente("Super", "Admin", "admin@system.com", 99, 9999.99, TipoUtente.ADMIN);
@@ -60,6 +67,63 @@ public class Application {
         utenteDAO.save(u20);
         */
 
+
+        // creo punti di emissioni
+
+//        try {
+//            System.out.println("Inizio creazione dei distributori e rivenditori...");
+//
+//            PuntoDiEmissione rivenditore1 = new Rivenditore("Via Roma 15, Milano");
+//            ((Rivenditore) rivenditore1).setNome("Tabaccheria Rossi");
+//
+//            PuntoDiEmissione rivenditore2 = new Rivenditore("Piazza Garibaldi 2, Napoli");
+//            ((Rivenditore) rivenditore2).setNome("Edicola Stazione");
+//
+//            PuntoDiEmissione rivenditore3 = new Rivenditore("Corso Vittorio Emanuele 88, Bari");
+//            ((Rivenditore) rivenditore3).setNome("Bar dello Sport");
+//
+//            PuntoDiEmissione rivenditore4 = new Rivenditore("Via Dante 42, Firenze");
+//            ((Rivenditore) rivenditore4).setNome("Tabacchi n.4");
+//
+//            PuntoDiEmissione rivenditore5 = new Rivenditore("Via dei Fori Imperiali 5, Roma");
+//            ((Rivenditore) rivenditore5).setNome("Edicola Central");
+//
+//            //
+//            PuntoDiEmissione distributore1 = new Distributore(StatoDistributore.DISPONIBILE, "Stazione Termini, Roma");
+//            PuntoDiEmissione distributore2 = new Distributore(StatoDistributore.DISPONIBILE, "Stazione Cadorna, Milano");
+//            PuntoDiEmissione distributore3 = new Distributore(StatoDistributore.DISPONIBILE, "Piazza Aldo Moro, Bari");
+//            PuntoDiEmissione distributore4 = new Distributore(StatoDistributore.DISPONIBILE, "Via Toledo 110, Napoli");
+//            PuntoDiEmissione distributore5 = new Distributore(StatoDistributore.NON_DISPONIBILE, "Fermata Tram 14, Torino");
+//
+//
+//            dao.SalvaPuntoDiEmissione(rivenditore1);
+//            dao.SalvaPuntoDiEmissione(rivenditore2);
+//            dao.SalvaPuntoDiEmissione(rivenditore3);
+//            dao.SalvaPuntoDiEmissione(rivenditore4);
+//            dao.SalvaPuntoDiEmissione(rivenditore5);
+//
+//            dao.SalvaPuntoDiEmissione(distributore1);
+//            dao.SalvaPuntoDiEmissione(distributore2);
+//            dao.SalvaPuntoDiEmissione(distributore3);
+//            dao.SalvaPuntoDiEmissione(distributore4);
+//            dao.SalvaPuntoDiEmissione(distributore5);
+//
+//            System.out.println("Tutti i distributori e rivenditori sono stati elaborati con successo!");
+//
+//        } catch (Exception e) {
+//            System.err.println("Errore durante il popolamento dei punti di emissione: " + e.getMessage());
+//            e.printStackTrace();
+//        } finally {
+//
+//            entityManager.close();
+//            entityManagerFactory.close();
+//        }
+//
+//
+
+
         System.out.println("Hello World!");
     }
+
+
 }
